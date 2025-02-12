613b005b452855e61ae08b3993b0b607c29801c4
Prateek jain <prateek32177@gmail.com>
2025-02-13 00:08:45 +0530

    temporary nav of other pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import HeaderAuth from '@/components/header-auth';
-import { ThemeSwitcher } from '@/components/theme-switcher';
 import { ThemeProvider } from 'next-themes';
 import Link from 'next/link';
 import './globals.css';
@@ -40,6 +39,14 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <nav className='flex h-16 w-full justify-center border-b border-b-foreground/10'>
+            <div className='flex w-full max-w-5xl items-center justify-between p-3 px-5 text-sm'>
+              <div className='flex items-center gap-5 font-semibold'>
+                <Link href={'/'}>BuzzLabs</Link>
+              </div>
+              {<HeaderAuth />}
+            </div>
+          </nav>
           <main className='flex min-h-screen flex-col flex-1 gap-20  items-center '>
             <div className=' max-w-5xl p-5 '>{children}</div>
           </main>

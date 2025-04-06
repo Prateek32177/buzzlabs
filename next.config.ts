5372cf70d4c418c251522fe5899bc8a7aca28b27
Prateek jain <prateek32177@gmail.com>
2025-04-06 15:25:55 +0530

    security policies removed

--- next.config.ts
+++ next.config.ts
@@ -1,65 +1,7 @@
-const cspHeader = `
-  default-src 'self';
-  script-src 'self' 'unsafe-inline';
-  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-  img-src 'self' data: blob: https:;
-  font-src 'self' https: https://fonts.gstatic.com;
-  object-src 'none';
-  base-uri 'self';
-  form-action 'self';
-  frame-ancestors 'none';
-  connect-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com;
-  media-src 'self';
-  worker-src 'self';
-  manifest-src 'self';
-  frame-src 'none';
-  upgrade-insecure-requests;
-`;
+import type { NextConfig } from 'next';
 
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ''),
-          },
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=0, must-revalidate',
-          },
-        ],
-      },
-      {
-        source: '/_next/static/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-      {
-        source: '/static/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-      {
-        source: '/:all*(svg|jpg|png|webp|avif|gif|ico)',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=3600, stale-while-revalidate=59',
-          },
-        ],
-      },
-    ];
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
+
+export default nextConfig;

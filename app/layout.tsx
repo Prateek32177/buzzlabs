8e9b74fa04157ad6d8288958fff1139c00062efe
Prateek Jain <49508975+Prateek32177@users.noreply.github.com>
2025-04-06 15:19:36 +0530

    Added metadata, SEO and security policies (#28)
    * meta data, robots.txt, header policies added
    
    * manifest updated
    
    * google font policy added
    
    * vercel deployment rule changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,17 @@ export const metadata = {
     creator: '@Prateek53788134',
     site: '@Prateek53788134',
   },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 };
 export default function RootLayout({
   children,

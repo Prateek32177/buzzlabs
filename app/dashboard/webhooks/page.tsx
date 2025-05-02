d79040ceca0d4eddf88ec4b8189df6d2dd29057e
Prateek jain <prateek32177@gmail.com>
2025-05-02 20:57:13 +0530

    minor fixes

--- app/dashboard/webhooks/page.tsx
+++ app/dashboard/webhooks/page.tsx
@@ -1,7 +1,7 @@
 import { WebhookManagement } from '@/components/dashboard/WebhookManagement';
 export default function WebhooksPage() {
   return (
-    <div className='space-y-6 mx-10'>
+    <div className='space-y-6 px-4 md:px-10 max-w-full'>
       <div className='flex flex-col gap-2'>
         <h1 className='text-3xl font-bold tracking-tight'>Welcome back</h1>
         <p className='text-zinc-400'>
--- components/dashboard/NotificationLogs/NotificationLogs.tsx
+++ components/dashboard/NotificationLogs/NotificationLogs.tsx
@@ -616,9 +616,9 @@ export function NotificationLogs() {
                                           viewBox='0 0 24 24'
                                           fill='none'
                                           stroke='currentColor'
-                                          stroke-width='2'
-                                          stroke-linecap='round'
-                                          stroke-linejoin='round'
+                                          strokeWidth='2'
+                                          strokeLinecap='round'
+                                          strokeLinejoin='round'
                                           className='lucide lucide-copy'
                                         >
                                           <rect

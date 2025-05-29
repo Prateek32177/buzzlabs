7db5838384ef3660eeb74fdfe33365dfce44f082
Prateek jain <prateek32177@gmail.com>
2025-05-30 01:01:59 +0530

    buid fix

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -14,7 +14,7 @@ import { SupabaseLogo, ClerkLogo } from '../Logos';
 import { StripeWordmarkLogo } from '@/components/Logos/StripeLogo';
 import { GitHubLogoIcon } from '@radix-ui/react-icons';
 import { DodoLogo } from '../Logos/DodoPayments';
-import { SignInDialog } from '@/components/auth/signInDialog';
+import { SignInDialog } from '@/components/auth/SIgnInDialog';
 import { Button } from '../ui/button';
 
 export default function Hero() {

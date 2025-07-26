e7b61330e46b3740e2a050a64fb96f15575d3dee
Prateek jain <prateek32177@gmail.com>
2025-07-26 18:50:08 +0530

    dialog updated

--- components/auth/SignInForm.tsx
+++ components/auth/SignInForm.tsx
@@ -9,7 +9,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
-import { ArrowRight, Github } from 'lucide-react';
+import { ArrowRight, Github, MailIcon } from 'lucide-react';
 import {
   Card,
   CardContent,
@@ -59,7 +59,9 @@ export function SignInForm({
           </CardHeader>
         )}
 
-        <CardContent className='space-y-6 pb-0'>
+        <CardContent
+          className={` ${isDialog ? 'pb-12 space-y-4' : 'pb-0 space-y-6'}`}
+        >
           <Button
             variant='outline'
             type='button'
@@ -97,73 +99,90 @@ export function SignInForm({
             Continue with GitHub
           </Button>
 
-          <div className='relative'>
-            <div className='absolute inset-0 flex items-center'>
-              <Separator className='w-full bg-zinc-700' />
-            </div>
-            <div className='relative flex justify-center text-[11px] uppercase tracking-wider'>
-              <span className='bg-zinc-900 px-2 text-zinc-500'>
-                or continue with
-              </span>
-            </div>
-          </div>
-
-          <div className='flex flex-col space-y-4'>
-            <div className='flex flex-col space-y-1'>
-              <Label htmlFor='email' className='text-xs text-zinc-400 '>
-                Email
-              </Label>
-              <Input
-                name='email'
-                placeholder='you@example.com'
-                required
-                className='rounded-xl bg-zinc-800 border border-zinc-700 focus:border-[#7B6FF1] focus:ring-1 focus:ring-[#7B6FF1] text-sm placeholder:text-zinc-500 transition'
-              />
-            </div>
-
-            <div className='flex flex-col space-y-1'>
-              <div className='flex items-center justify-between'>
-                <Label htmlFor='password' className='text-xs text-zinc-400'>
-                  Password
-                </Label>
-                <Link
-                  href='/forgot-password'
-                  className='text-xs font-medium text-[#9E8DF7] hover:text-[#7B6FF1]'
-                >
-                  Forgot?
-                </Link>
-              </div>
-              <Input
-                type='password'
-                name='password'
-                placeholder='Your password'
-                required
-                className='rounded-xl bg-zinc-800 border border-zinc-700 focus:border-[#7B6FF1] focus:ring-1 focus:ring-[#7B6FF1] text-sm placeholder:text-zinc-500 transition'
-              />
-            </div>
-
-            {searchParams && <FormMessage message={searchParams} />}
-          </div>
-        </CardContent>
-
-        <CardFooter
-          className={`flex justify-between space-x-3   ${isDialog ? 'pt-4' : 'pt-2'}`}
-        >
-          {showSignUpLink && (
-            <Button variant='outline' asChild>
-              <Link href='/sign-up'>Sign up</Link>
+          {isDialog ? (
+            <Button
+              variant='outline'
+              type='button'
+              className='w-full flex items-center justify-center gap-3 py-4 bg-zinc-800/60 border border-zinc-700 hover:bg-zinc-700/70 transition rounded-xl text-sm font-medium shadow-sm'
+              asChild
+            >
+              <Link href='/sign-in'>
+                <MailIcon className='h-4 w-4' />
+                Continue with Email
+              </Link>
             </Button>
+          ) : (
+            <>
+              <div className='relative'>
+                <div className='absolute inset-0 flex items-center'>
+                  <Separator className='w-full bg-zinc-700' />
+                </div>
+                <div className='relative flex justify-center text-[11px] uppercase tracking-wider'>
+                  <span className='bg-zinc-900 px-2 text-zinc-500'>
+                    or continue with
+                  </span>
+                </div>
+              </div>
+
+              <div className='flex flex-col space-y-4'>
+                <div className='flex flex-col space-y-1'>
+                  <Label htmlFor='email' className='text-xs text-zinc-400 '>
+                    Email
+                  </Label>
+                  <Input
+                    name='email'
+                    placeholder='you@example.com'
+                    required
+                    className='rounded-xl bg-zinc-800 border border-zinc-700 focus:border-[#7B6FF1] focus:ring-1 focus:ring-[#7B6FF1] text-sm placeholder:text-zinc-500 transition'
+                  />
+                </div>
+
+                <div className='flex flex-col space-y-1'>
+                  <div className='flex items-center justify-between'>
+                    <Label htmlFor='password' className='text-xs text-zinc-400'>
+                      Password
+                    </Label>
+                    <Link
+                      href='/forgot-password'
+                      className='text-xs font-medium text-[#9E8DF7] hover:text-[#7B6FF1]'
+                    >
+                      Forgot?
+                    </Link>
+                  </div>
+                  <Input
+                    type='password'
+                    name='password'
+                    placeholder='Your password'
+                    required
+                    className='rounded-xl bg-zinc-800 border border-zinc-700 focus:border-[#7B6FF1] focus:ring-1 focus:ring-[#7B6FF1] text-sm placeholder:text-zinc-500 transition'
+                  />
+                </div>
+
+                {searchParams && <FormMessage message={searchParams} />}
+              </div>
+            </>
           )}
-          <SubmitButton
-            className='rounded-xl bg-gradient-to-r from-[#9E8DF7] to-[#7B6FF1] text-white font-medium py-2 shadow-md hover:opacity-90 hover:scale-[1.02] active:scale-100 transition-transform'
-            pendingText='Signing in...'
-            formAction={signInAction}
-            aria-label='Sign in to your account'
+        </CardContent>
+        {!isDialog && (
+          <CardFooter
+            className={`flex justify-between space-x-3   ${isDialog ? 'pt-4' : 'pt-2'}`}
           >
-            Sign in
-            <ArrowRight className='ml-1 h-4 w-4 group-hover:translate-x-1 transition-transform' />
-          </SubmitButton>
-        </CardFooter>
+            {showSignUpLink && (
+              <Button variant='outline' asChild>
+                <Link href='/sign-up'>Sign up</Link>
+              </Button>
+            )}
+            <SubmitButton
+              className='rounded-xl bg-gradient-to-r from-[#9E8DF7] to-[#7B6FF1] text-white font-medium py-2 shadow-md hover:opacity-90 hover:scale-[1.02] active:scale-100 transition-transform'
+              pendingText='Signing in...'
+              formAction={signInAction}
+              aria-label='Sign in to your account'
+            >
+              Sign in
+              <ArrowRight className='ml-1 h-4 w-4 group-hover:translate-x-1 transition-transform' />
+            </SubmitButton>
+          </CardFooter>
+        )}
       </Card>
     </form>
   );
